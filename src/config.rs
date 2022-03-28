4ba3926d191d823b07aaf1958e0e2d9959122662
Lemon Rose <78662983+japandotorg@users.noreply.github.com>
2022-03-28 16:22:17 +0530

    configuration init

---
+++ src/config.rs
@@ -0,0 +1,40 @@
+use figment::Figment;
+use figment::providers::{
+    Format,
+    Toml,
+    Env
+};
+use serde::Deserialize;
+
+#[derive(Deserialize, Clone, Debug)]
+pub struct ApiConfig {
+    pub host: String,
+    pub port: i16,
+    pub redis: Redis,
+    pub webhook: Webhook,
+    pub auth: Auth,
+}
+
+#[derive(Deserialize, Clone, Debug)]
+pub struct Webhook {
+    pub url: String,
+    pub color: i32,
+}
+
+#[derive(Deserialize, Clone, Debug)]
+pub struct Auth {
+    pub top_gg: String,
+}
+
+#[derive(Deserialize, Clone, Debug)]
+pub struct Redis {
+    pub host: String,
+    pub port: i16,
+}
+
+pub fn load_config() -> Result<ApiConfig, figment::Error> {
+    Figment::new()
+        .merge(Toml::file("voting.toml"))
+        .merge(Env::prefixed("API_"))
+        .extract()
+}
